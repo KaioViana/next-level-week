ffe2a2ff47c9532bbcf4aa753f1a442bbdc46f95
KaioViana <kaiu.viana@hotmail.com>
2020-08-06 21:46:03 -0300

    added cors and List classes corrected

--- proffy/server/src/routes.ts
+++ proffy/server/src/routes.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import db from './database/connection'
-import convertHourToMinutes from './utils/convertHourToMinutes'
+import ClassesControler from './controllers/ClassesController'
+import ConnectionsController from './controllers/ConnectionsController'
 
 
 interface ScheduleItem {
@@ -10,59 +10,14 @@ interface ScheduleItem {
 }
 
 const routes = express.Router()
+const classesControler = new ClassesControler()
+const connectionsControler = new ConnectionsController()
 
-routes.post('/classes', async (req, res) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = req.body
 
-    const trx = await db.transaction()
-    
-    try {
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio,
-        })
+routes.get('/classes', classesControler.index)
+routes.post('/classes', classesControler.create)
 
-        const user_id = insertedUsersIds[0];
-
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id
-        })
-
-        const class_id = insertedClassesIds[0]
-
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to)
-            }
-        })
-
-        await trx('class_schedule').insert(classSchedule)
-
-        await trx.commit()
-
-        return res.status(201).send()
-    } catch(err) {
-        await trx.rollback()
-
-        return res.status(400).json({
-            error: "Unexpected error while creating new class"
-        })
-    }
-})
+routes.post('/connections', connectionsControler.create)
+routes.get('/connections', connectionsControler.index)
 
 export default routes
