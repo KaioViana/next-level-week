f48f1b86234287691f301780f783220cae75af81
KaioViana <kaiu.viana@hotmail.com>
2021-07-02 19:31:57 -0300

    finalizado

--- letmeask/src/pages/AdminRoom.tsx
+++ letmeask/src/pages/AdminRoom.tsx
@@ -5,6 +5,8 @@ import { useAuth } from '../hooks/useAuth'
 import { useRoom } from '../hooks/useRoom'
 import { Question } from '../components/Question'
 import logoImg from '../assets/images/logo.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
 import deleteImg from '../assets/images/delete.svg'
 import '../styles/rooms.scss'
 import { database } from '../services/firebase'
@@ -31,8 +33,20 @@ export function AdminRoom() {
     history.push('/')
   }
 
+  async function handleCheckQuestionAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true
+    })
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true
+    })
+  }
+
   async function handleDeleteQuestion(questionId: string) {
-    if(window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
     }
   }
@@ -43,8 +57,8 @@ export function AdminRoom() {
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
           <div>
-            <RoomCode code={roomId}/>
-            <Button 
+            <RoomCode code={roomId} />
+            <Button
               onClick={handleEndRoom}
               isOutlined
             >
@@ -56,13 +70,13 @@ export function AdminRoom() {
 
       <main className="content">
         <div className="room-title">
-          <h1>Sala {title}t</h1>
+          <h1>Sala {title}</h1>
           {
             questions.length > 0 &&
             <span>{questions.length} pergunta(s)</span>
           }
         </div>
-        
+
         <div className="question-list">
           {
             questions.map(question => {
@@ -71,13 +85,34 @@ export function AdminRoom() {
                   key={question.id}
                   content={question.content}
                   author={question.author}
+                  isAnswered={question.isAnswered}
+                  isHighlighted={question.isHighlighted}
                 >
+                  {
+                    !question.isAnswered &&
+                    (
+                      <>
+                        <button
+                          type="button"
+                          onClick={() => handleCheckQuestionAnswered(question.id)}
+                        >
+                          <img src={checkImg} alt="Marca pergunta como respondida" />
+                        </button>                  <button
+                          type="button"
+                          onClick={() => handleHighlightQuestion(question.id)}
+                        >
+                          <img src={answerImg} alt="Dar destaque à pergunta" />
+                        </button>
+                      </>
+                    )
+                  }
+
                   <button
                     type="button"
                     onClick={() => handleDeleteQuestion(question.id)}
                   >
-                    <img src={deleteImg} alt="Remover pergunta"/>
-                  </button> 
+                    <img src={deleteImg} alt="Remover pergunta" />
+                  </button>
                 </Question>
               )
             })
